a9a62b22ee6008e5b2d410aa05b89071ab8f7f71
Amarjeet-97 <amarjeetkumardmg8409@gmail.com>
2022-10-20 16:52:51 +0530

    paragraph added

--- src/components/App.js
+++ src/components/App.js
@@ -8,14 +8,15 @@ function App() {
   };
   return (
     <div id="main">
-      <button id="click" onClick={para}>
+      <button onClick={para} id="click">
         Show
       </button>
       <paragraph id="para" style={visi}>
-        {" "}
         Hello, I've learnt to use the full-stack evaluation tool. This makes me
         so happy
       </paragraph>
     </div>
   );
 }
+
+export default App;
